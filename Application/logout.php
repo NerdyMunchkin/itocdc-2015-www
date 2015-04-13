35fd5e50d982cd713d742adc82ad4aab71f3158b
jk314159 <jimmyk3.14159@gmail.com>
2015-04-13 17:49:35 -0500

    Reverted to original with exception of 3600 turned to 7200

--- Application/logout.php
+++ Application/logout.php
@@ -1,6 +1,5 @@
 <?php
 // clear session
-setcookie("PHPSESSID", authenticated_session($email), time()-7200);
-setcookie("user", $email, time()-7200);
+setcookie("PHPSESSID", "", time()-7200);
 header('Location: /index.php');
 ?>
