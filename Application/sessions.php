4e92554f16629838b33a03cbe63dcbdb1d7610ec
Nathan <nathang567@gmail.com>
2015-04-15 09:07:57 -0500

    Function to test authentication
    Added a function that looks for a file on the server with the name of the session ID. If it exists, the email is returned. Else, false is returned.

--- Application/sessions.php
+++ Application/sessions.php
@@ -2,4 +2,14 @@
   function authenticated_session($passwd) {
     return hash("sha256", hash("sha256", passwd));
   }
+  
+  function is_authenticated($id) {
+    if(file_exists($id)){
+      $userfile = fopen($id, "r");
+      return fread($userfile, filesize($userfile));
+    }else{
+      return false;
+    }
+  }
+  
 ?>
