e9c1c0cd9a9bed033a15b066946d1e5cbf936108
Jacob Moody <sinful.spartan@gmail.com>
2015-04-15 11:18:02 -0500

    Fixed Variable Names is openbd.php.
    The variables are now properly pointed towards the variables in config.php file that the program uses.

--- Application/opendb.php
+++ Application/opendb.php
@@ -1,6 +1,6 @@
 <?php
 // opens the database connection
-$db = new PDO("mysql:host=$DATABASE_IP;dbname=$DATABASE_NAME", $username, $password);
+$db = new PDO("mysql:host=$DATABASE_IP;dbname=$DATABASE_NAME", $DATABASE_USERNAME, $DATABASE_PASSWORD);
 
 //TODO: Error handling
 ?>
