dbb4d6112cb3cadc9d1166dd02c4f80898ed3832
Nathan <nathang567@gmail.com>
2015-04-15 10:00:18 -0500

    Removed authentication code with function
    The new algorithm is located in session.php

--- Application/authenticate.php
+++ Application/authenticate.php
@@ -11,25 +11,7 @@ $password=$_POST["password"];
 include 'config.php';
 include 'opendb.php';
 
-$query = $db->prepare('SELECT * FROM users WHERE email=:email AND password=:password');
-$query->bindParam(':email', $email, PDO::PARAM_STR, strlen($email));
-$query->bindParam(':password', $password, PDO::PARAM_STR, strlen($password));
-$query->execute();
-
-// authenticate user
-$login = $query->rowCount();
-
-if($login){
-  // set an active cookie for this username
-  setcookie("PHPSESSID", authenticated_session($email), time()+3600);
-  setcookie("user", $email, time()+3600);
-  header('Location: /index.php');
-} else {
-  // logout
-  setcookie("PHPSESSID", authenticated_session($email), time()-7200);
-  setcookie("user", $email, time()-7200);
-  header('Location: /login.php?message=Login%20Failed');
-}
+authenticate($email, $passwd);
 
 // close connection to the database
 include 'closedb.php';
