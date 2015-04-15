d2b3b21b1f80e7660451da5fa8a80e207cec07e8
Nathan <nathang567@gmail.com>
2015-04-15 09:57:50 -0500

    Wrote a new authenticate routine
    It creates a file on the server with the name of the user's session id containing the user's email, and then the client stores a cookie containing the session id.

--- Application/sessions.php
+++ Application/sessions.php
@@ -3,6 +3,29 @@
     return hash("sha256", hash("sha256", passwd));
   }
   
+  function authenticate($email, $passwd) {
+    $query = $db->prepare('SELECT * FROM users WHERE email=:email AND password=:password');
+    $query->bindParam(':email', $email, PDO::PARAM_STR, strlen($email));
+    $query->bindParam(':password', $password, PDO::PARAM_STR, strlen($password));
+    $query->execute();
+    
+    $login = $query->rowCount();
+    if($login){
+      $sessid = authenticated_session($passwd);
+      if(file_exists($sessid)){
+        header('Location: /login.php?message=Already%20logged%20in%20elsewhere');
+      }else{
+        $userfile = fopen($sessid, "w");
+        fwrite($userfile, $email);
+        setcookie("PHPSESSID", $sessid, time()+3600);
+        header('Location: /index.php');
+      }
+    } else {
+      setcookie("PHPSESSID", authenticated_session($passwd), time()-7200);
+      header('Location: /login.php?message=Login%20Failed');
+    }
+  }
+  
   function is_authenticated($id) {
     if(file_exists($id)){
       $userfile = fopen($id, "r");
