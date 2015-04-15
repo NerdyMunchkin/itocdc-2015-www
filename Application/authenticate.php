8910c1dd0ab10188a23d361333bac119e5a86742
Nathan <nathang567@gmail.com>
2015-04-15 15:10:26 -0500

    Exception handling in authentication
    Surrounded the script with a try/catch block.

--- Application/authenticate.php
+++ Application/authenticate.php
@@ -11,24 +11,29 @@ $password=$_POST["password"];
 include 'config.php';
 include 'opendb.php';
 
-$query = $db->prepare('SELECT * FROM users WHERE email=:email AND password=:password');
-$query->bindParam(':email', $email, PDO::PARAM_STR, strlen($email));
-$query->bindParam(':password', $password, PDO::PARAM_STR, strlen($password));
-$query->execute();
-
-// authenticate user
-$login = $query->rowCount();
-
-if($login){
-  // set an active cookie for this username
-  setcookie("PHPSESSID", authenticated_session($email), time()+3600);
-  setcookie("user", $email, time()+3600);
-  header('Location: /index.php');
-} else {
-  // logout
-  setcookie("PHPSESSID", authenticated_session($email), time()-7200);
-  setcookie("user", $email, time()-7200);
-  header('Location: /login.php?message=Login%20Failed');
+try{
+  $query = $db->prepare('SELECT * FROM users WHERE email=:email AND password=:password');
+  $query->bindParam(':email', $email, PDO::PARAM_STR, strlen($email));
+  $query->bindParam(':password', $password, PDO::PARAM_STR, strlen($password));
+  $query->execute();
+  
+  // authenticate user
+  $login = $query->rowCount();
+  
+  if($login){
+    // set an active cookie for this username
+    setcookie("PHPSESSID", authenticated_session($email), time()+3600);
+    setcookie("user", $email, time()+3600);
+    header('Location: /index.php');
+  } else {
+    // logout
+    setcookie("PHPSESSID", authenticated_session($email), time()-7200);
+    setcookie("user", $email, time()-7200);
+    header('Location: /login.php?message=Login%20Failed');
+  }
+} catch(Exception $e){
+  header("Location: /post.php?message=" . urlencode("Error: " . $e));
+  exit();
 }
 
 // close connection to the database
