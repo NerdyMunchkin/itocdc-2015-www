14159712f616f478edadfbd1dab767df7ad58d55
Jacob Moody <sinful.spartan@gmail.com>
2015-04-17 22:01:56 -0500

    Authenticate now hands off the un-hashed password as per the new evauluation in sessions.php

--- Application/authenticate.php
+++ Application/authenticate.php
@@ -12,14 +12,12 @@ include 'config.php';
 include 'opendb.php';
 include 'password.php';
 
-$hashPass = password_hash($password, PASSWORD_DEFAULT);
-
 if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
   //header('Location: /login.php?message=Invalid%20input');
   exit();
 }
 
-authenticate($email, $hashPass);
+authenticate($email, $password);
 
 // close connection to the database
 include 'closedb.php';
