eb12309ef4b12db0bd12c0ace1f61e7e434bc024
Nathan <nathang567@gmail.com>
2015-04-24 12:38:30 -0500

    added close quote
    -jimmy

--- Application/index.php
+++ Application/index.php
@@ -73,7 +73,7 @@
                     } ?>    
                     <li><a href="/login.php">Login</a></li>
                     <li><a href="/registration.php">Register</a></li>
-                    <li><a href="/user.php?username=<?php echo($userID); ?>>Your Profile</a></li>
+                    <li><a href="/user.php?username=<?php echo($userID); ?>">Your Profile</a></li>
                   <?php endif; ?>
                 <?php else: ?>
                   <li><a href="/login.php">Login</a></li>
