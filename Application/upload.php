0088a3da9f372db11ea55c2757b8a7073baca912
Nathan <nathang567@gmail.com>
2015-04-24 14:07:24 -0500

    Added check for enough storage

--- Application/upload.php
+++ Application/upload.php
@@ -14,62 +14,75 @@ function generateShortName($file) {
 if ($_FILES["video"]["error"] == UPLOAD_ERR_OK) {
     if(isset($_POST["video"]) || isset($_POST["title"]) || isset($_POST["description"])){
         if($_FILES["video"]["size"] < 100000000){
-            if(is_authenticated($_COOKIE["PHPSESSID"])){
-                // get filename
-                $filename = $_FILES["video"]["name"];
-              
-                // generate unique shortname for upload
-                $shortname = generateShortName($_FILES["video"]["tmp_name"]);
-                $extension = pathinfo($_FILES["video"]["name"], PATHINFO_EXTENSION);
-                if(file_exists($uploadDir . "/" . $shortname . "." .$extension)){
-                  header("Location: /post.php?message=" . urlencode("Video already posted."));
-                  exit();
-                }
-              
-                // check file type
-                if(in_array($_FILES["video"]["type"], $validMedia) != 1 || in_array($extension, $validMediaExtensions) != 1) {
-                  header("Location: /post.php?message=" . urlencode("File format not supported."));
-                  exit();
-                }
+            $email = is_authenticated($_COOKIE["PHPSESSID"]);
+            if($email){
+                // check disk quota
+                $query = $db->prepare('SELECT id FROM users WHERE email=:email');
+                $query->bindParam(':email', $email, strlen($email));
+                $query->execute();
+                $userRow = $query->fetch();
+                $userID = $userRow[0];
+                $useddisk = $userRow[DATA_COL];
                 
-                // move file to upload directory
-                move_uploaded_file($_FILES["video"]["tmp_name"], $uploadDir . "/" . $shortname . "." .$extension);
-              
-                // check upload success
-                if(!file_exists($uploadDir . "/" . $shortname . "." .$extension)){
-                  header("Location: /post.php?message=" . urlencode("Upload failed."));
-                  exit();
-                }
-              
-               
-              
-                // save input fields
-                $email = get_email($_COOKIE["PHPSESSID"]);
-                $title = filter_var($_POST["title"], FILTER_SANITIZE_STRING);
-                $description = filter_var($_POST["description"], FILTER_SANITIZE_STRING);
-                try {
-                  $query = $db->prepare('SELECT id FROM users WHERE email=:email');
-                  $query->bindParam(':email', $email, strlen($email));
-                  $query->execute();
-                  $userRow = $query->fetch();
-                  $userID = $userRow[0];
-            
-                  // insert video into clips table
-                  //$insertResult = mysql_query("INSERT INTO clips (host, shortname, title, description, user, extension) VALUES ('$APPLICATION_HOSTNAME', '$shortname', '$title', '$description', '$userID', '$extension')");
-                  $query = $db->prepare("INSERT INTO clips (host, shortname, title, description, user, extension) VALUES (:hostname, :shortname, :title, :description, :userID, :extension)");
-                  $query->bindParam(':hostname', $APPLICATION_HOSTNAME, strlen($APPLICATION_HOSTNAME));
-                  $query->bindParam(':shortname', $shortname, strlen($shortname));
-                  $query->bindParam(':title', $title, strlen($shortname));
-                  $query->bindParam(':description', $description, strlen($description));
-                  $query->bindParam(':userID', $userID, strlen($userID));
-                  $query->bindParam(':extension', $extension, strlen($extension));
-                  $query->execute();
-                  //TODO: add SQL error handling
-                  header("Location: /view.php?video=" . $shortname);
-                  exit();
-                } catch (Exception $e) {
-                  header("Location: /post.php?message=" . urlencode("Error: " . $e));
-                  exit();
+                if($useddisk < 1000000000){
+                    // get filename
+                    $filename = $_FILES["video"]["name"];
+                  
+                    // generate unique shortname for upload
+                    $shortname = generateShortName($_FILES["video"]["tmp_name"]);
+                    $extension = pathinfo($_FILES["video"]["name"], PATHINFO_EXTENSION);
+                    if(file_exists($uploadDir . "/" . $shortname . "." .$extension)){
+                      header("Location: /post.php?message=" . urlencode("Video already posted."));
+                      exit();
+                    }
+                  
+                    // check file type
+                    if(in_array($_FILES["video"]["type"], $validMedia) != 1 || in_array($extension, $validMediaExtensions) != 1) {
+                      header("Location: /post.php?message=" . urlencode("File format not supported."));
+                      exit();
+                    }
+                    
+                    // move file to upload directory
+                    move_uploaded_file($_FILES["video"]["tmp_name"], $uploadDir . "/" . $shortname . "." .$extension);
+                  
+                    // check upload success
+                    if(!file_exists($uploadDir . "/" . $shortname . "." .$extension)){
+                      header("Location: /post.php?message=" . urlencode("Upload failed."));
+                      exit();
+                    }
+                  
+                   
+                  
+                    // save input fields
+                    $title = filter_var($_POST["title"], FILTER_SANITIZE_STRING);
+                    $description = filter_var($_POST["description"], FILTER_SANITIZE_STRING);
+                    try {
+                      $query = $db->prepare('SELECT id FROM users WHERE email=:email');
+                      $query->bindParam(':email', $email, strlen($email));
+                      $query->execute();
+                      $userRow = $query->fetch();
+                      $userID = $userRow[0];
+                
+                      // insert video into clips table
+                      //$insertResult = mysql_query("INSERT INTO clips (host, shortname, title, description, user, extension) VALUES ('$APPLICATION_HOSTNAME', '$shortname', '$title', '$description', '$userID', '$extension')");
+                      $query = $db->prepare("INSERT INTO clips (host, shortname, title, description, user, extension) VALUES (:hostname, :shortname, :title, :description, :userID, :extension)");
+                      $query->bindParam(':hostname', $APPLICATION_HOSTNAME, strlen($APPLICATION_HOSTNAME));
+                      $query->bindParam(':shortname', $shortname, strlen($shortname));
+                      $query->bindParam(':title', $title, strlen($shortname));
+                      $query->bindParam(':description', $description, strlen($description));
+                      $query->bindParam(':userID', $userID, strlen($userID));
+                      $query->bindParam(':extension', $extension, strlen($extension));
+                      $query->execute();
+                      //TODO: add SQL error handling
+                      header("Location: /view.php?video=" . $shortname);
+                      exit();
+                    } catch (Exception $e) {
+                      header("Location: /post.php?message=" . urlencode("Error: " . $e));
+                      exit();
+                    }
+                } else{
+                    header("Location: /post.php?message=" . urlencode("You have used all your storage."));
+                    exit();
                 }
             } else {
                 header("Location: /post.php?message=" . urlencode("Unauthenticated user."));
