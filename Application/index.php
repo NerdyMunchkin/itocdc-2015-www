5607883a01c6a985a152f3eec5875dd4bd803aee
Nathan <nathang567@gmail.com>
2015-04-24 12:25:12 -0500

    Added Person User Profile Link
    this is Jimmy, by the way

--- Application/index.php
+++ Application/index.php
@@ -61,8 +61,19 @@
                     <li><a href="/post.php">Post Video</a></li>
                     <li><a href="/logout.php">Logout</a></li>
                   <?php else: ?>
+                    <?php $logged_in_email = is_authenticated($_COOKIE["PHPSESSID"]);
+                    $query = $db->prepare("SELECT id FROM users WHERE email=:email");
+                    $query->bindParam(':email', $logged_in_email, strlen($logged_in_email));
+                    $query->execute();
+                    if($query->rowCount() == 0){
+                      $userID = NULL;
+                    } else {
+                      $userRow = $query->fetch();
+                      $userID = $userRow[0];
+                    } ?>    
                     <li><a href="/login.php">Login</a></li>
                     <li><a href="/registration.php">Register</a></li>
+                    <li><a href="/user.php?username=<?php echo($userID); ?>>Your Profile</a></li>
                   <?php endif; ?>
                 <?php else: ?>
                   <li><a href="/login.php">Login</a></li>
