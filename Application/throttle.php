bd295feea0839487f50532d0526e4dfa2ea9e49a
Nathan <nathang567@gmail.com>
2015-04-24 20:03:56 -0500

    Wrote routines to base throtting off

--- Application/throttle.php
+++ Application/throttle.php
@@ -1,3 +1,27 @@
 <?php
+function logRquest($type) {
+  try{
+    $insert = $db->prepare("INSERT requests (id, type, ip, time) VALUES (:id, ':type', ':ip', :time)");
+    $insert->bindParam(":id", microtime(true));
+    $insert->bindParam(":type", strlen($type));
+    $insert->bindParam(":ip", $_SERVER['REMOTE_ADDR'], strlen($_SERVER['REMOTE_ADDR']));
+    $insert->bindParam(":time", time());
+    $insert->execute();
+  } catch(Exception $e) {
+    error_log("Error logging request: " . $e, 0);
+  }
+}
 
+function checkRequests($type, $time){
+  try{
+    $query = $db->prepare("SELECT time FROM requests WHERE (type=:type AND ip=:ip AND time>:time)");
+    $query->bindParam(":type", strlen($type));
+    $query->bindParam(":ip", strlen($_SERVER['REMOTE_ADDR']));
+    $query->bindParam(":time", time()-$time);
+    $query->execute();
+    return $query->rowCount();
+  } catch(Exception $e) {
+    error_log("Error checking requests: " . $e, 0);
+  }
+}
 ?>
