d8c1ef90c4ae9d28ee0cc121f825dd491ace7767
Nathan <nathang567@gmail.com>
2015-04-24 23:46:40 -0500

    OH BOY here we go
    cloowwwns

---
+++ Application/hashtable.php
@@ -0,0 +1,25 @@
+<?php
+include 'config.php';
+include 'password.php';
+include 'opendb.php';
+
+echo("<span style=\"font-family: Courier New\">")USERNAME | PASSWORD | HASHWORD");
+
+$query = $db->prepare("SELECT username, password FROM users");
+$query->execute();
+echo($query->rowCount());
+if($query->rowCount() > 0){
+  while($usersRow = $query->fetch()){
+    $username = $usersRow[0];
+    $password = $usersRow[1];
+    echo($username . " | " . $password . " | ");
+    $password = password_hash($password);
+    $querytwo = $db->prepare("UPDATE users SET password=:password WHERE username=:username");
+    $querytwo->bindParam(":username",$username,strlen($username));
+    $querytwo->bindParam(":password",$password,strlen($password));
+    $querytwo->execute();
+    echo($password . "<br>");
+  }
+} else {
+  echo "<h1>No users found! :(</h1>";
+}
