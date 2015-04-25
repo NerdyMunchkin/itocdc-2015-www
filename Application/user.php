a5679ba39f1f32cbd527353d08052ffb0510b461
Nathan <nathang567@gmail.com>
2015-04-24 21:50:25 -0500

    searchbox

--- Application/user.php
+++ Application/user.php
@@ -98,6 +98,10 @@
                     <li><a href="/post.php">Post Video</a></li>
                     <li><a href="/logout.php">Logout</a></li>
                     <li><a href="/user.php?username=<?php echo($username); ?>">Your Profile</a></li>
+                    <li><form name=search action="search.php" method="post">
+                    <input type="text" name="q">
+                    <input value="Search" type="submit">
+                    </form></li>
                   <?php else: ?>
                     <li><a href="/login.php">Login</a></li>
                     <li><a href="/registration.php">Register</a></li>
