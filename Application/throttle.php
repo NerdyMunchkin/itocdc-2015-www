723f826665f128bf9aa1b7a38cb710815222bb58
Nathan <nathang567@gmail.com>
2015-04-24 22:03:06 -0500

    Open and close db

--- Application/throttle.php
+++ Application/throttle.php
@@ -1,12 +1,14 @@
 <?php
 function logRquest($type) {
   try{
+    include 'opendb.php';
     $insert = $db->prepare("INSERT requests (id, type, ip, time) VALUES (:id, ':type', ':ip', :time)");
     $insert->bindParam(":id", microtime(true));
     $insert->bindParam(":type", strlen($type));
     $insert->bindParam(":ip", $_SERVER['REMOTE_ADDR'], strlen($_SERVER['REMOTE_ADDR']));
     $insert->bindParam(":time", time());
     $insert->execute();
+    include 'closedb.php';
   } catch(Exception $e) {
     error_log("Error logging request: " . $e, 0);
   }
@@ -14,11 +16,13 @@ function logRquest($type) {
 
 function checkRequests($type, $time){
   try{
+    include 'opendb.php';
     $query = $db->prepare("SELECT time FROM requests WHERE (type=:type AND ip=:ip AND time>:time)");
     $query->bindParam(":type", strlen($type));
     $query->bindParam(":ip", strlen($_SERVER['REMOTE_ADDR']));
     $query->bindParam(":time", time()-$time);
     $query->execute();
+    include 'closedb.php';
     return $query->rowCount();
   } catch(Exception $e) {
     error_log("Error checking requests: " . $e, 0);
